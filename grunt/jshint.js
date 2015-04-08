02e4a5e369918496891609fd365eb0c0e7017b83
Adam Hope <ahope@thoughtworks.com>
2015-04-08 15:17:24 +1000

    improving jshint

--- grunt/jshint.js
+++ grunt/jshint.js
@@ -1,5 +1,15 @@
 module.exports = function (grunt, options) {
   return {
+    options: {
+      reporter: require('jshint-stylish'),
+      curly: false,
+      eqeqeq: true,
+      eqnull: true,
+      browser: true,
+      globals: {
+        jQuery: true
+      },
+    },
     all: [
       'Gruntfile.js',
       'grunt/*.js',
