e1818a2e0d143eb4fa7b2e7cbb507677c75701db
Adam Hope <ahope@thoughtworks.com>
2015-04-09 13:39:27 +1000

    conncet config for staging environment

--- grunt/connect.js
+++ grunt/connect.js
@@ -1,13 +1,17 @@
-'use strict';
-
-module.exports = function (grunt, options) {
-  return {
-    server: {
-      options: {
-        port: 9001,
-        base: 'public',
-        keepalive: true
-      }
+module.exports = {
+  dev: {
+    options: {
+      port: 9001,
+      base: 'public',
+      keepalive: true,
+      livereload: true
     }
-  };
+  },
+  staging: {
+    options: {
+      port: 8080,
+      base: 'public',
+      keepalive: true
+    }
+  }
 };
