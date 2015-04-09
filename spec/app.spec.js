e162f61cd2ab2901a301c0236a1a243fe015ca40
Adam Hope <ahope@thoughtworks.com>
2015-04-09 13:57:46 +1000

    jasmine specs can now be run through jshint

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,8 +1,13 @@
+'use strict';
+
 var App = require('../src/js/app.js');
 
 describe('App', function() {
+
     var app = new App();
+
     it('should have a start function', function() {
         expect(app.start).toBeDefined();
     });
+
 });
