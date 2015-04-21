45fcd189b5bc5d5562d3f5de488767a9fd22320f
James Lewis <jamesl@thoughtworks.com>
2015-04-21 14:20:49 +1000

    add in the jquery ref to home spec

--- spec/views/homePage.spec.js
+++ spec/views/homePage.spec.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var HomePage = require('../../src/js/views/homePage'),
-  Router = require('../../src/js/router.js');
-  //$ = require('jQuery');
+  Router = require('../../src/js/router.js'),
+  $ = require('jQuery');
 
 global.router = new Router();
 
