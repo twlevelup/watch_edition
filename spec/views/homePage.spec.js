5e538cade531c81f573e7f3a74808c4f54681891
James Lewis <jamesl@thoughtworks.com>
2015-04-21 14:19:24 +1000

    add back in home spec

--- spec/views/homePage.spec.js
+++ spec/views/homePage.spec.js
@@ -1,11 +1,11 @@
-//'use strict';
-//
-//var HomePage = require('../../src/js/views/homePage'),
-//  Router = require('../../src/js/router.js');
-//  //$ = require('jQuery');
-//
-//global.router = new Router();
-//
-//describe('The Home Page', function() {
-//
-//});
+'use strict';
+
+var HomePage = require('../../src/js/views/homePage'),
+  Router = require('../../src/js/router.js');
+  //$ = require('jQuery');
+
+global.router = new Router();
+
+describe('The Home Page', function() {
+
+});
