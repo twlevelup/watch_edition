8572f67a7056d4688271f63a5153c468b913d837
James Lewis <jamesl@thoughtworks.com>
2015-04-21 14:23:28 +1000

    uncomment contacts spec and change case of jquery ref

--- spec/views/contactsPage.spec.js
+++ spec/views/contactsPage.spec.js
@@ -1,63 +1,63 @@
-//'use strict';
-//
-//var ContactsPage = require('../../src/js/views/contactsPage'),
-//  Router = require('../../src/js/router.js');
-//  //$ = require('jQuery');
-//
-//global.router = new Router();
-//
-//xdescribe('The Contacts Page', function() {
-//
-//  var contactsPage, router;
-//
-//  beforeEach(function () {
-//    router = new Router();
-//    contactsPage = new ContactsPage();
-//  });
-//
-//  describe('contacts data', function () {
-//
-//    it('should have a contacts collection', function () {
-//      expect(contactsPage.contactsCollection).toBeDefined();
-//    });
-//
-//    describe('loading data', function () {
-//      // it('')
-//    });
-//
-//  });
-//
-//  describe('button events', function () {
-//
-//    beforeEach(function () {
-//      spyOn(global.router, 'navigate');
-//    });
-//
-//    describe('button-right', function () {
-//
-//      it('should take you to the hoe page', function () {
-//
-//        var buttonRightAction = contactsPage.buttonEvents['button-right'];
-//        contactsPage[buttonRightAction]();
-//        expect(global.router.navigate).toHaveBeenCalledWith('', true);
-//      });
-//
-//    });
-//  });
-//
-//  describe('rendering', function () {
-//
-//    beforeEach(function () {
-//      contactsPage.$el = $('div');
-//      contactsPage.contactsCollection.reset([{}, {}, {}]);
-//    });
-//
-//    //it('should render each of the contacts', function () {
-//    //  spyOn(contactsPage, 'createContactHTML');
-//    //  contactsPage.render();
-//    //  expect(contactsPage.createContactHTML.calls.count()).toEqual(3);
-//    //});
-//
-//  });
-//
-//});
+'use strict';
+
+var ContactsPage = require('../../src/js/views/contactsPage'),
+  Router = require('../../src/js/router.js'),
+  $ = require('jquery');
+
+global.router = new Router();
+
+describe('The Contacts Page', function() {
+
+  var contactsPage, router;
+
+  beforeEach(function () {
+    router = new Router();
+    contactsPage = new ContactsPage();
+  });
+
+  describe('contacts data', function () {
+
+    it('should have a contacts collection', function () {
+      expect(contactsPage.contactsCollection).toBeDefined();
+    });
+
+    describe('loading data', function () {
+      // it('')
+    });
+
+  });
+
+  describe('button events', function () {
+
+    beforeEach(function () {
+      spyOn(global.router, 'navigate');
+    });
+
+    describe('button-right', function () {
+
+      it('should take you to the hoe page', function () {
+
+        var buttonRightAction = contactsPage.buttonEvents['button-right'];
+        contactsPage[buttonRightAction]();
+        expect(global.router.navigate).toHaveBeenCalledWith('', true);
+      });
+
+    });
+  });
+
+  describe('rendering', function () {
+
+    beforeEach(function () {
+      contactsPage.$el = $('div');
+      contactsPage.contactsCollection.reset([{}, {}, {}]);
+    });
+
+    //it('should render each of the contacts', function () {
+    //  spyOn(contactsPage, 'createContactHTML');
+    //  contactsPage.render();
+    //  expect(contactsPage.createContactHTML.calls.count()).toEqual(3);
+    //});
+
+  });
+
+});
