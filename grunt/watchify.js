e3f362e0af4214789e9b208689a4900ef22857ca
James Lewis <jamesl@thoughtworks.com>
2015-04-22 17:39:39 +1000

    Moved vendor code to separate file and made them globals

--- grunt/watchify.js
+++ grunt/watchify.js
@@ -6,6 +6,30 @@ module.exports = {
     callback: function(b) {
       b.transform('hbsfy');
       return b;
+    },
+    external: ['jquery', 'underscore', 'backbone']
+  },
+  vendor: {
+    src: ['./src/js/vendor.js'],
+    dest: 'public/js/vendor.js',
+    options: {
+      shim: {
+        jquery: {
+          path: 'node_modules/jquery/src/jquery.js',
+          exports: '$'
+        },
+        underscore: {
+          path: 'node_modules/underscore/underscore.js',
+          exports: '_'
+        },
+        backbone: {
+          path: 'node_modules/backbone/backbone.js',
+          exports: 'Backbone',
+          depends: {
+            underscore: 'underscore'
+          }
+        }
+      }
     }
   },
   test: {
---
+++ src/js/vendor.js
@@ -0,0 +1,3 @@
+global.$ = require('jquery');
+global._ = require('underscore');
+global.Backbone = require('backbone');
