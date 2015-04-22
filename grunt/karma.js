d2d8537b225fc0b8b997518dc1faf29b320b8da5
Adam Hope <adam@adamhope.com>
2015-04-22 20:42:12 +1000

    Added Karma config

---
+++ grunt/karma.js
@@ -0,0 +1,12 @@
+module.exports = {
+    options: {
+        configFile: 'karma.conf.js'
+    },
+    watcher: {
+        background: true,
+        singleRun: false
+    },
+    test: {
+        singleRun: true
+    }
+};
