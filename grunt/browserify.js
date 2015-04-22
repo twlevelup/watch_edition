2c97b8e01db34dcda233c0e348a176172df5a9f7
Adam Hope <adam@adamhope.com>
2015-04-22 21:30:40 +1000

    Simplifying browserify config

--- grunt/browserify.js
+++ grunt/browserify.js
@@ -1,26 +1,6 @@
 'use strict';
 
 module.exports = {
-  options: {
-    debug: true
-  },
-  // vendor: {
-  //   src: ['./src/js/vendor.js'],
-  //   dest: 'public/js/vendor.js',
-  // },
-  // // FIXME THIS IS LAME
-  // testVendor: {
-  //   src: ['./src/js/vendor.js'],
-  //   dest: 'build/vendor.js',
-  // },
-  // test: {
-  //   src: './spec/**/*.spec.js',
-  //   dest: 'build/spec-bundle.js',
-  //   options: {
-  //     transform: ['hbsfy'],
-  //     external: ['jquery', 'underscore', 'backbone']
-  //   }
-  // },
   app: {
     src: [
       './src/js/main.js',
@@ -29,7 +9,6 @@ module.exports = {
     dest: './public/js/main.js',
     options: {
       transform: ['hbsfy'],
-      // external: ['jquery', 'underscore', 'backbone']
     }
   }
 };
