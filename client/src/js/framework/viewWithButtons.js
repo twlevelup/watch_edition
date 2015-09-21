55d4152161003de0670175cb8181906c5b536cbb
Adam Hope <ahope@thoughtworks.com>
2015-09-21 22:03:07 +1000

    Adding alias for setButtonEvents

--- client/src/js/framework/viewWithButtons.js
+++ client/src/js/framework/viewWithButtons.js
@@ -2,11 +2,17 @@
 
 var viewWithButtons = Backbone.View.extend({
 
+  // TODO deprecate this
   setButtonEvents: function() {
 
     _.each(this.buttonEvents, function(eventHandler, buttonID) {
       this.listenTo(global.App.vent, buttonID, this[eventHandler]);
     }, this);
+  },
+
+  // TODO make this the standard method name
+  configureButtons: function () {
+    this.setButtonEvents();
   }
 
 });
