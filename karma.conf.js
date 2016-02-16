a4cb629b34edc7db5be8616b950cd094abc9aced
Shuqian Hon <sqhon@thoughtworks.com>
2016-02-16 18:57:59 +1100

    Load webpack config into karma conf.
    Pending coverage/threshold reporting

--- karma.conf.js
+++ karma.conf.js
@@ -1,88 +1,89 @@
 'use strict';
 
-var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
+webpackConfig.entry = {};
 
-module.exports = function(config) {
-    config.set({
+module.exports = function (config) {
+  config.set({
 
-        // base path, that will be used to resolve files and exclude
-        basePath: '',
-
-        // frameworks to use
-        // frameworks: ['jasmine-jquery', 'jasmine'],
-
-
-        // list of files / patterns to load in the browser
-        // NOTE: do NOT include jasmine here because grunt-karma already does
-        files: ['client/spec/**/*spec.js'],
-
-        // list of files to exclude
-        exclude: [],
-
-        // test results reporter to use
-        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-        reporters: ['spec', 'coverage', 'threshold'],
-
-        coverageReporter: {
-          'type' : 'text',
-          'dir' : 'coverage/',
-          //comment this out if you want to see the output in the console
-          'file': 'coverageResult.txt',
-          watermarks: {
-            statements: [ 60, 90 ],
-            functions: [ 60, 90 ],
-            branches: [ 60, 80 ],
-            lines: [ 60, 90 ]
-          }
-        },
-
-        // the configure thresholds
-        thresholdReporter: {
-          statements: 80,
-          branches: 70,
-          functions: 80,
-          lines: 80
-        },
-
-        // web server port
-        port: 9876,
-
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
-
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+    webpack: webpackConfig,
+    // frameworks to use
+    frameworks: ['jasmine-jquery', 'jasmine'],
 
 
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: false,
+    // list of files / patterns to load in the browser
+    // NOTE: do NOT include jasmine here because grunt-karma already does
+    files: ['client/spec/**/*spec.js'],
+
+    // list of files to exclude
+    exclude: [],
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['spec', 'coverage', 'threshold'],
+
+    coverageReporter: {
+      'type': 'text',
+      'dir': 'coverage/',
+      //comment this out if you want to see the output in the console
+      'file': 'coverageResult.txt',
+      watermarks: {
+        statements: [60, 90],
+        functions: [60, 90],
+        branches: [60, 80],
+        lines: [60, 90]
+      }
+    },
+
+    // the configure thresholds
+    thresholdReporter: {
+      statements: 80,
+      branches: 70,
+      functions: 80,
+      lines: 80
+    },
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-        // Start these browsers, currently available:
-        // - Chrome
-        // - ChromeCanary
-        // - Firefox
-        // - Opera (has to be installed with `npm install karma-opera-launcher`)
-        // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-        // - PhantomJS
-        // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-        browsers: ['PhantomJS'],
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
 
-        // If browser does not capture in given timeout [ms], kill it
-        captureTimeout: 60000,
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['PhantomJS'],
 
 
-        // Continuous Integration mode
-        // if true, it capture browsers, run tests and exit
-        singleRun: true,
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
 
-        preprocessors: {
-          'client/spec/**/*spec.js': ['webpack', 'sourcemap'],
-          'client/src/js/main.js': ['webpack', 'sourcemap']
-        }
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true,
 
-    });
+
+    preprocessors: {
+      'client/spec/**/*spec.js': ['webpack', 'sourcemap'],
+      'client/src/js/main.js': ['webpack', 'sourcemap']
+    }
+
+  });
 };
