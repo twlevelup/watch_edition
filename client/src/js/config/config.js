d8fea3a7dc2d494c3e05af06ba9eae07ce22009c
Shuqian Hon <sqhon@thoughtworks.com>
2016-03-03 18:43:16 +1100

    Remove firebase config

--- client/src/js/config/config.js
+++ client/src/js/config/config.js
@@ -2,10 +2,4 @@
 
 var Config = {};
 
-if (process.env.NODE_ENV === 'production') {
-  Config.firebaseUrl = 'https://watch-edition-prod.firebaseIO.com';
-} else {
-  Config.firebaseUrl = 'https://watch-edition-dev.firebaseIO.com';
-}
-
 module.exports = Config;
