dd20d56f5057a4e5299846882aa2f029743e9e71
Sami <slenc@thoughtworks.com>
2018-03-08 11:14:41 +1100

    [Safira/Sami] Changed strap colour and watch font

--- client/src/js/notifications.js
+++ client/src/js/notifications.js
@@ -4,4 +4,14 @@ module.exports = [
     label: "Alert",
     defaultValue: "This is a test alert",
   },
+  {
+    notificationType: "random1",
+    label: "test1",
+    defaultValue: "This is a test alert",
+  },
+  {
+    notificationType: "random2",
+    label: "test2",
+    defaultValue: "This is a test alert",
+  },
 ];
