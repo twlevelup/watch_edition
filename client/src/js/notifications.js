ea22947845709bfb7032d5986739b2d69ea15dcb
Sami <slenc@thoughtworks.com>
2018-03-08 12:15:38 +1100

    [Sami/Safira] Make Notifications box responsive and add top margin to watch

--- client/src/js/notifications.js
+++ client/src/js/notifications.js
@@ -3,15 +3,5 @@ module.exports = [
     notificationType: "alert",
     label: "Alert",
     defaultValue: "This is a test alert",
-  },
-  {
-    notificationType: "random1",
-    label: "test1",
-    defaultValue: "This is a test alert",
-  },
-  {
-    notificationType: "random2",
-    label: "test2",
-    defaultValue: "This is a test alert",
-  },
+  }
 ];
