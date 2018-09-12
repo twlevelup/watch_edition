4fc388c3f378112f3287faa374fa2be09ca4a1b5
Sarang Leehan <slleehan@thoughtworks.com>
2018-09-12 15:29:40 +1000

    refactor AudioHub test

--- framework/spec/AudioHub.spec.js
+++ framework/spec/AudioHub.spec.js
@@ -4,29 +4,25 @@ describe("AudioHub", () => {
 
   let audioHub;
   const beep = './framework/src/sounds/sound.mp3'
+  const audioMock = { play: jest.fn() }; 
 
   beforeEach(() => {
     audioHub = new AudioHub();
+    window.Audio = jest.fn(() => audioMock);
   })
 
   it('should play a sound', () => {
-    const audioMock = { play: jest.fn() };
-    window.Audio = jest.fn(() => audioMock);
     audioHub.playSound(beep);
     expect(window.Audio).toBeCalledWith(beep);
     expect(audioMock.play).toHaveBeenCalled();
   })
 
   it('should set a volume to the sound', () => {
-    const audioMock = { play: jest.fn() };
-    window.Audio = jest.fn(() => audioMock);
     audioHub.playSound(beep, 0.5);
     expect(audioMock.volume).toEqual(0.5);
   })
 
   it('should throw an error when given a volume value outside of 0 and 1', () => {
-    const audioMock = { play: jest.fn() };
-    window.Audio = jest.fn(() => audioMock);
     expect(() => {audioHub.playSound(beep, 1.5)}).toThrow();
     expect(() => {audioHub.playSound(beep, -2)}).toThrow();  
   })
