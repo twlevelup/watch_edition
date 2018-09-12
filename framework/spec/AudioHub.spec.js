2fee6bd41addb8bed8ef494f8b7757cca0c16ef9
Sarang Leehan <slleehan@thoughtworks.com>
2018-09-12 15:26:40 +1000

    constrain volume parameter to be between 0 and 1

--- framework/spec/AudioHub.spec.js
+++ framework/spec/AudioHub.spec.js
@@ -23,4 +23,11 @@ describe("AudioHub", () => {
     audioHub.playSound(beep, 0.5);
     expect(audioMock.volume).toEqual(0.5);
   })
+
+  it('should throw an error when given a volume value outside of 0 and 1', () => {
+    const audioMock = { play: jest.fn() };
+    window.Audio = jest.fn(() => audioMock);
+    expect(() => {audioHub.playSound(beep, 1.5)}).toThrow();
+    expect(() => {audioHub.playSound(beep, -2)}).toThrow();  
+  })
 });
--- framework/src/AudioHub.js
+++ framework/src/AudioHub.js
@@ -2,10 +2,13 @@ class AudioHub {
 
   playSound(filepath, volume=1) {
     let sound = new Audio(filepath)
-    sound.volume = volume;
+    if (volume <= 1 && volume >= 0) {  
+      sound.volume = volume;
+    } else {
+      throw new Error("Enter volume between 0 and 1")
+    }
     sound.play();
   }
-  
 }
 
 module.exports = AudioHub;
