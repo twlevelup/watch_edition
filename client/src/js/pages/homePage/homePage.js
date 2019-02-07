86899cacdcf685ea347557871a826849fc72a9a1
Your Name <levelup.raspberry@gmail.com>
2019-02-07 16:32:05 +1100

    test

--- client/src/js/pages/homePage/homePage.js
+++ client/src/js/pages/homePage/homePage.js
@@ -4,6 +4,7 @@ const AudioHub = require('watch-framework').AudioHub;
 const logo = require('../../../images/logo.png');
 const plop = './sounds/plop.mp3';
 
+//test for raspberrypi
 class HomePage extends BasePage {
   template = require('./homePage.hbs');
 
