273905b6e6f12aae213d2e6c45a7164606c37e24
Christie Goh <gxychristie@gmail.com>
2019-03-21 22:53:17 +1100

    change map source
    add npm packages
    
    fix pkg json

--- framework/src/MyMap.js
+++ framework/src/MyMap.js
@@ -1,50 +1,25 @@
-// import Map from 'ol/Map.js';
-// import View from 'ol/View.js';
-// import TileLayer from 'ol/layer/Tile.js';
-// import BingMaps from 'ol/source/BingMaps.js';
-//const Handlebars = require('handlebars');
-//require("handlebars.element").default(Handlebars);
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-class MyMap extends HTMLElement {
+export default class Map extends HTMLElement {
     constructor() {
       console.log("wassup");
-        // constructCustomElement(MyMap);
         super();
-        var shadow = this.attachShadow({mode: 'open'});
-        
-        let s = document.createElement('div');
-        s.id = 'map';
-        s.className = 'map';
-        this.renderMap(s);
-        shadow.append(s);
+        // var mdiv = document.createElement('div');
+        // mdiv.id = 'map';
+        // document.getElementById('watch').appendChild(mdiv);
+        this.renderMap();
     }
-
-    renderMap(s){
-      console.log("rendering map...");
-      var layers = [];
-      layers.push(new TileLayer({
-          visible: false,
-          preload: Infinity,
-          source: new BingMaps({
-            key: 'AvCh6GkfMxiH6-hQigzgd-iBa2lIcq-N-0bwCbzl2276l3GBFEgccQxpCmEHfxP5',
-            imagerySet: 'Road'
-          })
-      }));
-      
-      var map = new Map({
-        layers: layers,
-        loadTilesWhileInteracting: true,
-        target: 'map',
-        view: new View({
-          center: [-6655.5402445057125, 6709968.258934638],
-          zoom: 13
-        })
-      });
-
-      //s.appendChild(map);
+    
+    renderMap(){
+      console.log("rendering map");
+      this.map = L.map('map',{
+        center: [33.8,151.20],
+        zoom: 0,
+        zoomControl: true
+      })
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(this.map);
     }
 }
-
-document.body.appendChild(document.createElement('MyMap'));
-//customElements.define('my-map', MyMap);
-module.exports = MyMap;
