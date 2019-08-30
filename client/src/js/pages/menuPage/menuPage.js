1c240b74b28fbf26075f439c39b2836500430511
Desiree Kelly <desireekelly@users.noreply.github.com>
2019-08-30 19:50:02 +1000

    Add menu

---
+++ client/src/js/pages/menuPage/menuPage.js
@@ -0,0 +1,61 @@
+const BasePage = require('watch-framework').BasePage;
+const StorageHub = require('watch-framework').StorageHub;
+Number.prototype.mod = function (n) {
+  return ((this % n) + n) % n;
+};
+
+class MenuPage extends BasePage {
+  template = require("./menuPage.hbs");
+  tracker = 0;
+  selected;
+  list;
+
+  pageWillLoad() {
+    this.menuDetails = StorageHub.getData("menuDetails");
+    document.getElementById("clock-time");
+  }
+
+  pageDidLoad(){
+    this.list = document.getElementsByClassName("menu");
+    this.selected = this.menuDetails[this.tracker];
+    this.selectItem();
+  }
+
+  selectItem(item = 0) {
+    const currentItem = this.list[item];
+    currentItem.setAttribute("data-selected", true);
+  }
+
+  unselectItem(item) {
+    const currentItem = this.list[item];
+    currentItem.setAttribute("data-selected", false);
+  }
+
+  move(direction) {
+    this.unselectItem(this.tracker);
+    const numMenuItems = this.menuDetails.length;
+    this.tracker = (this.tracker + direction).mod(numMenuItems)
+    this.selected = this.menuDetails[this.tracker];
+    this.selectItem(this.tracker);
+  }
+
+  rightButtonEvent() {
+    this.navigate("menuDetails");
+    StorageHub.setData("selected", this.selected);
+
+  }
+
+  leftButtonEvent() {
+    this.navigate("/");
+  }
+
+  topButtonEvent() {
+    this.move(-1);
+  }
+
+  bottomButtonEvent() {
+    this.move(1);
+  }
+}
+
+module.exports = MenuPage;
--- client/src/js/routes.js
+++ client/src/js/routes.js
@@ -2,10 +2,14 @@ const HomePage = require('./pages/homePage/homePage');
 const ContactsPage = require('./pages/contactsPage/contactsPage');
 const TeamPage = require('./pages/teamPage/teamPage');
 const FourOhFour = require('./pages/404Page/404Page');
+const MenuPage = require('./pages/menuPage/menuPage');
+const MenuDetailsPage = require('./pages/menuDetailsPage/menuDetailsPage');
 
 module.exports = {
   '/': HomePage,
   'contacts': ContactsPage,
   'team': TeamPage,
   '404': FourOhFour,
+  'menuDetails': MenuDetailsPage,
+  'menu': MenuPage,
 };
